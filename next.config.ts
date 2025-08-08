e5aa2c4bd17e1f4e015b19adffabb66cfd8ccde9
MD Pabel <mdpabel385@gmail.com>
2025-08-09 02:55:31 +0600

    feat: blog author

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'websiteblacklistremoval.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
     ],
   },
   async headers() {
