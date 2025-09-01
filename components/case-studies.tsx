859597ebc76531c6775a98264d5cb1841a868231
MD Pabel <mdpabel385@gmail.com>
2025-09-02 03:13:11 +0600

    fix: href

--- components/case-studies.tsx
+++ components/case-studies.tsx
@@ -18,7 +18,7 @@ const CaseStudies = async () => {
           <Link
             key={post.id}
             className='group flex justify-between items-center py-3 border-b border-b-slate-700/50 text-md text-slate-300/80 no-underline'
-            href={`/blog/${post.slug}`}>
+            href={`/case-studies/${post.slug}`}>
             <div
               className='w-[calc(100%-80px)] transition-transform group-hover:translate-x-2'
               dangerouslySetInnerHTML={{
