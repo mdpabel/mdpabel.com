bbb755c624806697d08e5648b1d18664edc00fbf
MD Pabel <mdpabel385@gmail.com>
2025-09-02 20:03:36 +0600

    chore: gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import Footer from '@/components/footer';
 import { Analytics } from '@vercel/analytics/next';
 import TawkTo from '@/components/tawk-chat';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className='scroll-smooth'>
+      <GoogleTagManager gtmId='GTM-W3CL327W' />
       <body>
         <div className='grid grid-rows-[auto_1fr_auto] bg-slate-900 w-full min-h-[100dvh]'>
           <Header />
